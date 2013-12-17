319f9e0165caa31c052b36cf748ed8e82d19a64b
Charles128 <charles.amoako.128@gmail.com>
2013-12-17 02:55:22 -0500

    basic html calculator set up. can only click and display numbers

--- calc.js
+++ calc.js
@@ -1,3 +1,22 @@
+var calculator = {
+  equation: [],
+
+  attachEventListeners: function(){
+    $('.number').on('click', function(){
+      $('#calc-display').append(this.innerHTML)
+      calculator.equation.push(parseInt(this.innerHTML))
+      console.log(calculator.equation)
+    })
+
+    $('#operands').on('click', 'div', function(){
+      $('#calc-display').append(this.innerHTML)
+      calculator.equation.push(this.innerHTML)
+      console.log(calculator.equation)
+    })
+  },
+
+}
+
 window.onload = function(){
-  console.log('ready')
+  calculator.attachEventListeners()
 }
